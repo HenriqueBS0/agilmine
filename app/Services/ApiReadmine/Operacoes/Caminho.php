a4b358f47e5f16e6b8ea103ab4451c3061dabe49
HenriqueBS0 <henrique.10agr@gmail.com>
2024-10-17 01:03:51 -0300

    feat(api-readmine-service): adicione classe Caminho para manipulação de parâmetros em caminhos
    Detalhes:
    - Implementou nova classe Caminho na pasta Operacoes
    - Adicionou método addParametro para adicionar parâmetros dinâmicos
    - Criou método resolver para substituir parâmetros no caminho inicial
    - Utiliza tratamento de tipos para garantir segurança de dados

---
+++ app/Services/ApiReadmine/Operacoes/Caminho.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Services\ApiReadmine\Operacoes;
+
+/**
+ * Classe Caminho
+ *
+ * Esta classe é responsável por manipular e substituir parâmetros em um caminho.
+ * Ela permite adicionar parâmetros dinâmicos ao caminho inicial e substituí-los posteriormente.
+ *
+ * @package App\Services\ApiReadmine\Operacoes
+ */
+
+class Caminho
+{
+    /**
+     * Armazena os parâmetros adicionados ao caminho.
+     *
+     * @var array<string, int|float|string>
+     */
+    private array $parametros = [];
+
+    /**
+     * Construtor da classe Caminho.
+     *
+     * @param string $caminho O caminho inicial que será usado como base para substituição de parâmetros.
+     */
+    public function __construct(
+        private string $caminho
+    ) {
+    }
+
+    /**
+     * Adiciona um parâmetro ao caminho.
+     *
+     * @param string $nome O nome do parâmetro a ser adicionado.
+     * @param int|float|string $valor O valor do parâmetro.
+     *
+     * @return static Instância atualizada da classe Caminho.
+     */
+    public function addParametro(string $nome, int|float|string $valor): static
+    {
+        $this->parametros[$nome] = $valor;
+        return $this;
+    }
+
+    /**
+     * Substitui os parâmetros no caminho inicial.
+     *
+     * @return string O caminho final com os parâmetros substituídos.
+     */
+    public function resolver(): string
+    {
+        $resultado = $this->caminho;
+        foreach ($this->parametros as $nome => $valor) {
+            $resultado = str_replace("{" . $nome . "}", (string) $valor, $resultado);
+        }
+        return $resultado;
+    }
+}
