a78f5e32eff57007462a6f2867220bb4c3257cbe
HenriqueBS0 <henrique.10agr@gmail.com>
2024-10-23 10:44:05 -0300

    feat(navbar): remove o a cor de background da navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar border-bottom">
     <div class="container-fluid justify-content-start">
         <span class="navbar-brand mb-0 h1">Agilmine</span>
         {{ $breadcrumb ?? '' }}
