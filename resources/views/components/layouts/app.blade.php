77b56abad14b36aa10d25f93840ef4f366981130
HenriqueBS0 <henrique.10agr@gmail.com>
2024-10-22 22:34:01 -0300

    feat(layout): criar layout de páginas

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="d-flex flex-column vh-100">
+    <div class="nav-container bg-danger">
+        Nav
+    </div>
+    <div class="content-contaienr h-100 d-flex">
+        <div class="sidebar-container bg-info col-3">
+            Sidebar
+        </div>
+        <main class="main-content bg-success col overflow-y-scroll">
+            Main
+        </main>
+    </div>
+</body>
+
+</html>
