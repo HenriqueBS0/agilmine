6c12ec00a8d60b959cd17371489409648ac2cb50
HenriqueBS0 <henrique.10agr@gmail.com>
2024-11-03 15:57:12 -0300

    feat(api-redmine: Cria a entidade perfil

---
+++ app/Services/ApiRedmine/Entidades/PerfilProjeto.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services\ApiRedmine\Entidades;
+use Livewire\Wireable;
+
+class PerfilProjeto implements Wireable
+{
+    /**
+     * Identificador
+     * @var int
+     */
+    private ?int $id = null;
+
+    /**
+     * Nome
+     * @var string
+     */
+    private ?string $nome = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(?string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toLivewire()
+    {
+        return [
+            'id' => $this->getId(),
+            'nome' => $this->getNome()
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function fromLivewire($value)
+    {
+        return (new self)
+            ->setId($value['id'])
+            ->setNome($value['nome']);
+    }
+}
