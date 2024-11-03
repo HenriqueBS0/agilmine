6d647ae54534c51a8ed06bfe444f9af6f3cf05cc
HenriqueBS0 <henrique.10agr@gmail.com>
2024-11-03 17:16:17 -0300

    feat: Criar a página de backlog da sprint

---
+++ app/Livewire/PaginaSprintBacklogDetalhamentoTarefa.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Services\ApiRedmine\ApiRedmine;
+use App\Services\ApiRedmine\Entidades\Membro;
+use App\Services\ApiRedmine\Entidades\Tarefa;
+use App\Services\ApiRedmine\Entidades\TarefaPrioridade;
+use App\Services\ApiRedmine\Entidades\TarefaStatus;
+use App\Services\ApiRedmine\Entidades\TarefaTipo;
+use Livewire\Component;
+
+class PaginaSprintBacklogDetalhamentoTarefa extends Component
+{
+
+    public ?int $identificador = null;
+
+    public ?string $titulo = null;
+
+    public ?int $prioridade = null;
+
+    public ?array $prioridades = null;
+
+    public ?int $storiePoints = null;
+
+    public ?int $tipo = null;
+
+    public ?array $tipos = null;
+
+    public ?int $status = null;
+
+    public ?array $statusList = null;
+
+    public ?float $proporcaoFeita = null;
+
+    public ?string $horasEstimadas = null;
+
+    public ?string $horasGastas = null;
+
+    public ?string $dataCriacao = null;
+
+    public ?string $dataUltimaAlteracao = null;
+
+    public ?string $dataPrevista = null;
+
+    public ?string $dataInicio = null;
+
+    public ?string $dataConclusao = null;
+
+    public ?int $autor = null;
+
+    public ?int $desenvolvedor = null;
+
+    public ?int $descritor = null;
+
+    public ?int $testador = null;
+
+    public ?array $membros = null;
+
+    public ?string $descricao = null;
+
+    public function mount(array $prioridades, array $tipos, array $statusList, array $membros)
+    {
+        $this->setPrioridades($prioridades);
+        $this->setTipos($tipos);
+        $this->setStatusList($statusList);
+        $this->setMembros($membros);
+    }
+
+    /**
+     * @param \App\Services\ApiRedmine\Entidades\TarefaPrioridade[] $prioridades
+     */
+    private function setPrioridades(array $prioridades): void
+    {
+        $this->prioridades = array_map(
+            fn(TarefaPrioridade $prioridade) => ['valor' => $prioridade->getId(), 'descricao' => $prioridade->getNome()],
+            $prioridades
+        );
+    }
+
+    /**
+     * @param \App\Services\ApiRedmine\Entidades\TarefaTipo[] $tipos
+     */
+    private function setTipos(array $tipos): void
+    {
+        $this->tipos = array_map(
+            fn(TarefaTipo $tipo) => ['valor' => $tipo->getId(), 'descricao' => $tipo->getNome()],
+            $tipos
+        );
+    }
+
+    /**
+     * @param \App\Services\ApiRedmine\Entidades\TarefaStatus[] $status
+     */
+    private function setStatusList(array $status): void
+    {
+        $this->statusList = array_map(
+            fn(TarefaStatus $status) => ['valor' => $status->getId(), 'descricao' => $status->getNome()],
+            $status
+        );
+    }
+
+    /**
+     * @param \App\Services\ApiRedmine\Entidades\Membro[] $membros
+     */
+    private function setMembros(array $membros): void
+    {
+        $fnMap = function (Membro $membro) {
+            return [
+                'valor' => $membro->getUsuario()->getId(),
+                'descricao' => $membro->getUsuario()->getNome()
+            ];
+        };
+
+        $this->membros = array_map($fnMap, $membros);
+    }
+
+    public function setTarefa(int $tarefaId)
+    {
+        if ($tarefaId === $this->identificador) {
+            return;
+        }
+
+        $tarefa = $this->fetchTarefa($tarefaId);
+
+        $this->identificador = $tarefa->getId();
+        $this->titulo = $tarefa->getTitulo();
+        $this->prioridade = $tarefa->getPrioridade()->getId();
+        $this->storiePoints = $tarefa->getPontosHistoria();
+        $this->tipo = $tarefa->getTipo()->getId();
+        $this->status = $tarefa->getStatus()->getId();
+        $this->proporcaoFeita = $tarefa->getProporcaoFeita();
+        $this->horasEstimadas = $tarefa->getStringHorasEstimadas();
+        $this->horasGastas = $tarefa->getStringHorasGastas();
+        $this->dataCriacao = $tarefa->getDataCriacao()?->format('Y-m-d');
+        $this->dataUltimaAlteracao = $tarefa->getDataAtualizacao()?->format('Y-m-d');
+        $this->dataPrevista = $tarefa->getDataConclusaoEstimada()?->format('Y-m-d');
+        $this->dataInicio = $tarefa->getDataInicio()?->format('Y-m-d');
+        $this->dataConclusao = $tarefa->getDataConclusao()?->format('Y-m-d');
+        $this->autor = $tarefa->getAutor()?->getId();
+        $this->desenvolvedor = $tarefa->getDesenvolvedor()?->getId();
+        $this->descritor = $tarefa->getDescritor()?->getId();
+        $this->testador = $tarefa->getDescritor()?->getId();
+        $this->descricao = $tarefa->getDescricao();
+    }
+
+    /**
+     * @param int $tarefaId
+     * @return Tarefa
+     */
+    private function fetchTarefa(int $tarefaId)
+    {
+        $parametros = Tarefa::parametroListar(1);
+        $parametros->filtro()->igual('issue_id', $tarefaId);
+        return ApiRedmine::listar($parametros)->dados()[0];
+    }
+
+    public function render()
+    {
+        return view('livewire.pagina-sprint-backlog-detalhamento-tarefa');
+    }
+}
---
+++ resources/views/livewire/pagina-sprint-backlog-detalhamento-tarefa.blade.php
@@ -0,0 +1,110 @@
+<div class="col-12" @tarefa-selecionada.window="onTarefaSelecionada($event)" x-data="{
+    tarefaSelecionada: null,
+    onTarefaSelecionada(event) {
+        const tarefaId = event.detail.tarefa.id;
+
+        if (tarefaId === this.tarefaSelecionada) {
+            return;
+        }
+
+        this.tarefaSelecionada = tarefaId;
+        $wire.setTarefa(event.detail.tarefa.id);
+    }
+}">
+    <h4>Tarefa</h4>
+    <div class="card">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="identificacao-tab" data-bs-toggle="tab"
+                        data-bs-target="#identificacao-tab-pane" type="button" role="tab"
+                        aria-controls="identificacao-tab-pane" aria-selected="true">Identificação</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="acompanhamento-tab" data-bs-toggle="tab"
+                        data-bs-target="#acompanhamento-tab-pane" type="button" role="tab"
+                        aria-controls="acompanhamento-tab-pane" aria-selected="true">Acompanhamento</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="membros-tab" data-bs-toggle="tab" data-bs-target="#membros-tab-pane"
+                        type="button" role="tab" aria-controls="membros-tab-pane"
+                        aria-selected="true">Membros</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="descricao-tab" data-bs-toggle="tab"
+                        data-bs-target="#descricao-tab-pane" type="button" role="tab"
+                        aria-controls="descricao-tab-pane" aria-selected="true">Descrição</button>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body tab-content">
+            <div class="tab-pane container active" id="identificacao-tab-pane" role="tabpanel"
+                aria-labelledby="identificacao-tab" tabindex="0">
+                <div class="row g-2">
+                    <x-input-number containerClass="col-1" label="Id" id="identificador" model="identificador"
+                        :disabled=true />
+                    <x-input-text containerClass="col-11" label="Titulo" id="titulo" model="titulo" :disabled=true />
+                    <x-input-select containerClass="col-2" label="Prioridade" id="prioridade" model="prioridade"
+                        :disabled=true :options="$prioridades" />
+                    <x-input-number containerClass="col-2" label="Storie Points" id="storiePoints" model="storiePoints"
+                        :disabled=true />
+                    <x-input-select containerClass="col-4" label="Tipo" id="tipo" model="tipo" :disabled=true
+                        :options="$tipos" />
+                    <x-input-select containerClass="col-4" label="Status" id="status" model="status" :disabled=true
+                        :options="$statusList" />
+                </div>
+            </div>
+            <div class="tab-pane container" id="acompanhamento-tab-pane" role="tabpanel"
+                aria-labelledby="acompanhamento-tab" tabindex="0">
+                <div class="row g-2">
+                    <x-input-number containerClass="col-2" label="% Terminado" id="proporcaoFeita"
+                        model="proporcaoFeita" :disabled=true />
+                    <x-input-text containerClass="col-2" label="Horas Estimadas" id="horasEstimadas"
+                        model="horasEstimadas" :disabled=true />
+                    <x-input-text containerClass="col-2" label="Horas Gastas" id="horasGastas" model="horasGastas"
+                        :disabled=true />
+                    <x-input-date containerClass="col-2" label="Data Criação" id="dataCriacao" model="dataCriacao"
+                        :disabled=true />
+                    <x-input-date containerClass="col-2" label="Data Ultima Alteração" id="dataUltimaAlteracao"
+                        model="dataUltimaAlteracao" :disabled=true />
+                    <x-input-date containerClass="col-2" label="Data Prevista" id="dataPrevista"
+                        model="dataPrevista" :disabled=true />
+                    <x-input-date containerClass="col-6" label="Data Inicio" id="dataInicio" model="dataInicio"
+                        :disabled=true />
+                    <x-input-date containerClass="col-6" label="Data Conclusão" id="dataConclusao"
+                        model="dataConclusao" :disabled=true />
+                </div>
+            </div>
+            <div class="tab-pane container" id="membros-tab-pane" role="tabpanel" aria-labelledby="membros-tab"
+                tabindex="0">
+                <div class="row g-2">
+                    <x-input-select containerClass="col-6" label="Autor" id="autor" model="autor"
+                        :disabled=true :options="$membros" />
+                    <x-input-select containerClass="col-6" label="Desenvolvedor" id="desenvolvedor"
+                        model="desenvolvedor" :disabled=true :options="$membros" />
+                    <x-input-select containerClass="col-6" label="Descritor" id="descritor" model="descritor"
+                        :disabled=true :options="$membros" />
+                    <x-input-select containerClass="col-6" label="Testador" id="testador" model="testador"
+                        :disabled=true :options="$membros" />
+                </div>
+            </div>
+            <div class="tab-pane" id="descricao-tab-pane" role="tabpanel" aria-labelledby="descricao-tab"
+                tabindex="0">
+                <p class="card-text overflow-y-auto" style="max-height: 200px">
+                    {{ $descricao }}
+                </p>
+            </div>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-sm text-bg-primary border-0" disabled><i class="bi bi-floppy"></i> Salvar</button>
+            @if ($identificador)
+                <a class="btn btn-sm btn-primary border-0" target="_blank"
+                    href="http://fabtec.ifc-riodosul.edu.br/issues/{{ $identificador }}"><i
+                        class="bi bi-box-arrow-up-right"></i> Acessar</a>
+            @else
+                <button class="btn btn-sm text-bg-primary border-0" disabled><i class="bi bi-box-arrow-up-right"></i>
+                    Acessar</button>
+            @endif
+        </div>
+    </div>
+</div>
