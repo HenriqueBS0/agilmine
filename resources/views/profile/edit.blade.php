74d6f3a17cbf15b57a827ae8b860683836a312ba
HenriqueBS0 <henrique.10agr@gmail.com>
2024-12-02 23:47:05 -0300

    feat(ui): adiciona modal de confirmação para exclusão de conta
    - Implementado modal de confirmação para a exclusão de conta, solicitando a senha do usuário.
    - Adicionado botão que abre o modal dentro da seção de exclusão de conta.
    - Formulário de exclusão foi transferido para o modal para maior segurança e clareza.
    
    Motivação:
    - Melhorar a experiência do usuário ao evitar exclusões acidentais.
    - Garantir maior segurança ao solicitar a confirmação com senha antes da exclusão definitiva.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -101,13 +101,58 @@
                                 <p class="text-danger">
                                     {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
                                 </p>
-                                <form method="post" action="{{ route('profile.destroy') }}">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger">{{ __('Delete Account') }}</button>
-                                </form>
+
+                                <!-- Botão que abre o modal -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#confirmDeletionModal">
+                                    {{ __('Delete Account') }}
+                                </button>
                             </div>
                         </div>
+
+                        <!-- Modal de Confirmação -->
+                        <div class="modal fade" id="confirmDeletionModal" tabindex="-1"
+                            aria-labelledby="confirmDeletionModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="confirmDeletionModalLabel">
+                                            {{ __('Delete Account') }}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>
+                                            {{ __('Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                                        </p>
+                                        <form id="deleteAccountForm" method="post"
+                                            action="{{ route('profile.destroy') }}">
+                                            @csrf
+                                            @method('delete')
+                                            <div class="mb-3">
+                                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                                                <input type="password" id="password" name="password"
+                                                    class="form-control" placeholder="{{ __('Password') }}" required>
+                                                @if ($errors->userDeletion->has('password'))
+                                                    <div class="text-danger mt-1">
+                                                        {{ $errors->userDeletion->first('password') }}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                            {{ __('Cancel') }}
+                                        </button>
+                                        <button type="submit" form="deleteAccountForm" class="btn btn-danger">
+                                            {{ __('Delete Account') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
