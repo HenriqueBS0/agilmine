b1be64ec74fe59140fad51d0c246be304b4217b4
HenriqueBS0 <henrique.10agr@gmail.com>
2024-12-22 17:51:11 -0300

    feat:466: registrar middleware persistente para Livewire
    Adicionado o `AdminMiddleware` como middleware persistente no Livewire através do `AppServiceProvider`. Essa alteração garante que as verificações administrativas sejam aplicadas consistentemente nas requisições do Livewire.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Livewire::addPersistentMiddleware([
+            AdminMiddleware::class
+        ]);
     }
 }
